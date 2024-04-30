f041cd321e610c7b684dc5c7d68718c584ae45fb
Abdulrehman Azam <abdulrehmana28@proton.me>
2024-04-30 09:26:55 +0500

    added custom theme
    by using copyWith & Theme widget for custom theme for a specific page

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,18 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
+/*
         primaryColor: const Color(0xFF0A0E21),
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFF0A0E21),
@@ -19,12 +30,7 @@ class BMICalculator extends StatelessWidget {
         textTheme: const TextTheme(
           // For the body text color in the app
           bodyMedium: TextStyle(color: Colors.white),
-        ),
-      ),
-      home: InputPage(),
-    );
-  }
-}
+        ), */
 
 class InputPage extends StatefulWidget {
   @override
@@ -46,9 +52,14 @@ class _InputPageState extends State<InputPage> {
           'Body Text',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+
+      // Custom theme for a specific widget
+      floatingActionButton: Theme(
+        data: ThemeData.dark(),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
