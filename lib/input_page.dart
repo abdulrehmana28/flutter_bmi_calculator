41c415909c3a2a59df69ed180d54f89388fbb83f
Abdulrehman Azam <abdulrehmana28@proton.me>
2024-05-01 10:27:04 +0500

    main UI Structure
    completed home page main UI structure

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,7 +16,6 @@ class _InputPageState extends State<InputPage> {
         elevation: 5.0,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Expanded(
             child: Row(
@@ -44,18 +43,64 @@ class _InputPageState extends State<InputPage> {
                 )
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color(0xFF1d1e33),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.all(15.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color(0xFF1d1e33),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.all(15.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: const Color(0xFF1d1e33),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
 
       // Custom theme for a specific widget
-      floatingActionButton: Theme(
+      /* floatingActionButton: Theme(
         data: ThemeData.dark(),
         child: FloatingActionButton(
           onPressed: () {},
           child: const Icon(Icons.add),
         ),
-      ),
+      ), */
     );
   }
 }
